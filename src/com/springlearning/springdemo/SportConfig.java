8a83eafbbd1ab1de0a223f518a7722161f3643c2
Divya Gupta <divygupta0319@gmail.com>
2021-05-09 17:52:00 +0530

    Spring Bean definition using Java source code only, and also Logging message class definition done for it

---
+++ src/com/springlearning/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.springlearning.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.springlearning.springdemo")
+public class SportConfig {
+
+}
