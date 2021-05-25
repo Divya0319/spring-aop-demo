615bca2f362cc087f7ea58c90c3db5154cc4a987
Divya Gupta <divygupta0319@gmail.com>
2021-05-25 13:36:52 +0530

    Added code for Logger and ConsoleHandler, created initial setup and starting files for AOP demo project

--- src/com/springlearning/springdemo/DemoConfig.java
+++ src/com/springlearning/springdemo/DemoConfig.java
@@ -9,12 +9,16 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:mylogger.properties")
-public class MyLoggerConfig {
+@EnableAspectJAutoProxy
+@ComponentScan("com.luv2code.aopdemo")
+public class DemoConfig {
 	
 
 		@Value("${root.logger.level}")
