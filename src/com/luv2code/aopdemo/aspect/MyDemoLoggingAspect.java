8e2c9dbf318ef0649c1f128ea1ad08eee92c29d7
Divya Gupta <divygupta0319@gmail.com>
2021-05-25 17:59:02 +0530

    Reusing and applying a pointcut to multiple advice(s) performed here

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,22 +2,33 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 	
+	
 	// this is where  we add all our related advices for logging
 	
 	// let's start with an @Before advice
 	
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
-		
 		
 		System.out.println("\n======>>>>  Executing @Before advice on method");
 		
+	}
+	
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n======>>>>  Performing API analytics");
 	}
 
 }
