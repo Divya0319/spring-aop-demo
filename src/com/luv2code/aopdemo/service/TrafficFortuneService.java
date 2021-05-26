08e9abe7fcf22929bfd144b409e8177c851ca5af
Divya Gupta <divygupta0319@gmail.com>
2021-05-26 18:44:57 +0530

    Using @Around advice to swallow the custom exception thrown, and never return that exception back to caller

--- src/com/luv2code/aopdemo/service/TrafficFortuneService.java
+++ src/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -23,4 +23,12 @@ public class TrafficFortuneService {
 		// return a fortune
 		return "Expect heavy traffic this morning";
 	}
+
+	public String getFortune(boolean tripWire) {
+		
+		if(tripWire) {
+			throw new RuntimeException("Major Accident! Highway Closed!");
+		}
+		return getFortune();
+	}
 }
